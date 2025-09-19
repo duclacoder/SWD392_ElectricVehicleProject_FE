df6b609f0b4b0ab147ab29e8125a769c365a5ce7
LaNam <namlbnse183845@fpt.edu.vn>
2025-09-19 16:03:05 +0700

    fix header and footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { Header } from "../../Widgets/Headers/Header.tsx";
 import { Footer } from "../../Widgets/Footers/Footer.tsx";
-import { HeroSection } from "./ui/HeroSection.tsx";
+import { Header } from "../../Widgets/Headers/Header.tsx";
 import FeaturesSession from "./ui/FeaturesSession.tsx";
+import { HeroSection } from "./ui/HeroSection.tsx";
 import MarketComponent from "./ui/MarketComponent.tsx";
 
 const HomePage = () => {
